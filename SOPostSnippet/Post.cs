902b2141d6f615230776ba590b76cff1fdae84e7
myasinK <yasinul@gmail.com>
2018-09-19 00:33:16 -0700

    Design StackOverflow post class skeleton

---
+++ SOPostSnippet/Post.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace SOPostSnippet
+{
+    public class Post
+    {
+        // needs to have props for title, description, date/time
+        // should be able to upvote and downvote
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; private set; }
+        public int Upvotes { get; private set; }
+        public int Downvotes { get; private set; }
+        private bool isPostValid;
+
+        public Post()
+        {
+            Upvotes = 0;
+            Downvotes = 0;
+            isPostValid = false;
+        }
+         
+        public int Upvote()
+        {
+            return isPostValid ? Upvotes++ : throw new Exception("Cannot vote on this post yet");
+        }
+
+        public int Downvote()
+        {
+            return isPostValid ? Downvotes++ : throw new Exception("Cannot vote on this post yet");
+        }
+
+        public void Publish(string title, string description)
+        {
+            if (!String.IsNullOrEmpty(title) && !(String.IsNullOrEmpty(description)))
+            {
+                DateTime = DateTime.Now;
+                Title = title;
+                Description = description;
+                isPostValid = true;
+                Console.WriteLine("Post published successfully");
+            }
+            else
+            {
+                throw new Exception("Your post is invalid");
+            }
+        }
+    }
+}
---
+++ SOPostSnippet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOPostSnippet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title, description;
+            title = "this is the title";
+            description = "this is the description";
+            //title = "";
+            //description = "";
+            var post = new Post();
+            post.Downvote();
+            post.Publish(title, description);
+            post.Upvote();
+            for (int i = 0; i < 8; i++)
+            {
+                post.Downvote();
+            }
+            Console.WriteLine("Up: {0}, Down: {1}", post.Upvotes, post.Downvotes);
+        }
+    }
+}
